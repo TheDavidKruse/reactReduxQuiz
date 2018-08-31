5d75b8e1f5ea39c7b54ca30476cce7313632030d
David Kruse <thedavidkruse@gmail.com>
2018-08-31 01:32:21 -0700

    had to change to hash router as I dont wanna mess with the config anymore

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ class App extends Component {
           <Route exact path="/post/new" component={NewPost}/>
           <Route exact path="/post/:id" component={PostDetail}/>
           <Route exact path="/author/:name" component={Author}/>
-          <Route exact path="/reactReduxQuiz" render={() => <Redirect to="/home"/>}/>
           <Route component={fourOhfour}/>
         </Switch>
       </div>
